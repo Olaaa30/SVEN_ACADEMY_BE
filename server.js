503b85312af9895850a6de8782d17a8150a21b16
Olayinka <adebisiolayinka30@gmail.com>
2023-08-28 15:20:56 +0100

    created basic routes

--- server.js
+++ server.js
@@ -1,10 +1,53 @@
-const express = require('express');
+require("dotenv/config");
+const express = require("express");
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
 const app = express();
+const mongoose = require("mongoose");
+
+app.use(morgan("tiny"));
+app.use(bodyParser.json());
+const api = process.env.API_URL;
 
 
-
-app.get('/', (req, res) =>{
-    res.send("Welcome to SVEN Academy!")
+const programSchema = mongoose.Schema({
+    title: String,
+    body: String,
+    image: String,
+    dateCreated: { type: Date, default: Date.now}
 })
 
-app.listen(3500, () => console.log("Server running on port 3500"));
+const Program = mongoose.model('Program', programSchema)
+
+
+
+app.get(`${api}/programs`, (req, res) => {
+  const product = {
+    id: 1,
+    name: "Youth Empowerment",
+    image: "some_url",
+  };
+  res.send(product);
+});
+
+app.post(`${api}/programs`, (req, res) =>{
+    const program = new Program({
+        title: req.body.title,
+        
+        image: req.body.image,
+    })
+    req.send(newProgram)
+})
+
+mongoose
+  .connect(process.env.DB_HOST)
+  .then(() => {
+    console.log("DATABASE CONNECTION IS READY...");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+app.listen(3000, () => {
+  console.log("Server running on http://localhost:3000");
+});
